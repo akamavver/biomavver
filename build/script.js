8fa1b63be15e4c4fb7e1f8afd5807ca2f0aa0144
akamavver <akamavver@mail.ru>
2026-03-30 23:36:22 +0300

    fix

--- build/script.js
+++ build/script.js
@@ -1,4 +1,4 @@
-const images = {{IMAGES}};
+const images = ["img_0.jpg", "img_1.jpg", "img_2.jpg", "img_3.jpg", "img_4.jpg", "img_5.jpg", "img_6.jpg", "img_7.jpg", "img_8.jpg", "img_9.jpg", "img_10.jpg", "img_11.jpg", "img_12.jpg", "img_13.jpg", "img_14.jpg", "img_15.jpg", "img_16.jpg", "img_17.jpg", "img_18.jpg", "img_19.jpg", "img_20.jpg", "img_21.jpg", "img_22.jpg", "img_23.jpg", "img_24.jpg", "img_25.jpg", "img_26.jpg", "img_27.jpg", "img_28.jpg", "img_29.jpg", "img_30.jpg", "img_31.jpg", "img_32.jpg", "img_33.jpg", "img_34.jpg", "img_35.jpg", "img_36.jpg", "img_37.jpg", "img_38.jpg", "img_39.jpg", "img_40.jpg", "img_41.jpg", "img_42.jpg", "img_43.jpg", "img_44.jpg", "img_45.jpg", "img_46.jpg", "img_47.jpg", "img_48.jpg", "img_49.jpg", "img_50.jpg", "img_51.jpg", "img_52.jpg", "img_53.jpg", "img_54.jpg", "img_55.jpg", "img_56.jpg", "img_57.jpg", "img_58.jpg", "img_59.jpg", "img_60.jpg", "img_61.jpg", "img_62.jpg", "img_63.jpg", "img_64.jpg", "img_65.jpg", "img_66.jpg", "img_67.jpg", "img_68.jpg"];
 const grid = document.getElementById('grid');
 
 let items = [];
